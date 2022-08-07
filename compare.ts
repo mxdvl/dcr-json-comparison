687710b8352924b991499a067f6d5f131d90b939
Max Duval <max.duval@theguardian.com>
2022-08-07 15:28:06 +0100

    feat(compare): record parsing failures & issues

--- compare.ts
+++ compare.ts
@@ -8,6 +8,18 @@ const wait = (n: number) =>
     setTimeout(resolve, n);
   });
 
+const failed_parsing = new Set<string>();
+const differences = new Set<string>();
+for (const [article, { zod, ajv }] of articles.entries()) {
+  if (typeof ajv === "boolean" && zod !== ajv) {
+    differences.add(article);
+  }
+  if (!zod) failed_parsing.add(article);
+}
+
+console.log("Difference:", differences.size, "/", articles.size);
+console.log("Failures:", failed_parsing.size, "/", articles.size);
+
 for await (const webUrl of getArticleUrls(100_000)) {
   try {
     if (articles.has(webUrl)) continue;
@@ -16,10 +28,13 @@ for await (const webUrl of getArticleUrls(100_000)) {
     const zod = checkZod(data);
     const ajv = checkAjv(data);
 
+    if (!zod) console.warn("Failed to parse:", webUrl);
+
     articles.set(webUrl, { zod, ajv });
   } catch (error) {
     console.warn("Something went wrong with", webUrl);
-    console.warn(error);
+    if (error instanceof SyntaxError) console.warn(error.message);
+
     issues.add(webUrl);
 
     const delay = 1200 + Math.random() * 6_000;
