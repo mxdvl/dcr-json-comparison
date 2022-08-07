db840d6c91b758f1b41c6c39aacd4059256bcc6f
Max Duval <max.duval@theguardian.com>
2022-08-07 11:46:14 +0100

    feat(dedupe): record issues

--- compare.ts
+++ compare.ts
@@ -1,5 +1,5 @@
 import { checkAjv } from "./ajv/article-fe.ts";
-import { articles } from "./dedupe.ts";
+import { articles, issues } from "./dedupe.ts";
 import { getArticleUrls, getContent } from "./get-articles.ts";
 import { checkZod } from "./zod/article-fe.ts";
 
@@ -8,16 +8,27 @@ const wait = (n: number) =>
     setTimeout(resolve, n);
   });
 
-for await (const webUrl of getArticleUrls(10_000)) {
-  if (articles.has(webUrl)) continue;
+for await (const webUrl of getArticleUrls(100_000)) {
+  try {
+    if (articles.has(webUrl)) continue;
 
-  const data = await getContent(webUrl);
-  const zod = checkZod(data);
-  const ajv = checkAjv(data);
+    const data = await getContent(webUrl);
+    const zod = checkZod(data);
+    const ajv = checkAjv(data);
 
-  articles.set(webUrl, { zod, ajv });
+    articles.set(webUrl, { zod, ajv });
+  } catch (error) {
+    console.warn("Something went wrong with", webUrl);
+    console.warn(error);
+    issues.add(webUrl);
 
-  await wait(12 + Math.random() * 240);
+    const delay = 1200 + Math.random() * 6_000;
+    console.warn(
+      `Waiting for ${(delay / 1000).toFixed(1)}s before checking the next item`
+    );
+
+    await wait(delay);
+  }
 }
 
 Deno.exit(0);
--- dedupe.ts
+++ dedupe.ts
@@ -1,4 +1,4 @@
-import type { infer as Infer } from "./zod/zod.ts";
+import { infer as Infer, set } from "./zod/zod.ts";
 import { map, object, string, boolean } from "./zod/zod.ts";
 
 const articlesSchema = map(
@@ -8,6 +8,8 @@ const articlesSchema = map(
     ajv: boolean().optional(),
   })
 );
+
+const issues_schema = set(string());
 
 type ArticlesMap = Infer<typeof articlesSchema>;
 
@@ -20,14 +22,34 @@ const getArticles = async (): Promise<ArticlesMap> => {
 };
 
 export const articles = await getArticles();
-console.log(articles.size, "articles checked (from disk)")
+console.log(articles.size, "articles checked (from disk)");
+
+const issues_file_path = new URL(import.meta.resolve("./issues.json"));
+const getIssues = async () => {
+  const input = await Deno.readTextFile(issues_file_path);
+  const array = JSON.parse(input);
+  return issues_schema.parse(new Set(array));
+};
+
+export const issues = await getIssues();
+console.log(issues.size, "issues recorded (from disk)");
 
 let lastSize = articles.size;
 setInterval(async () => {
-  if (articles.size === lastSize) return;
+  if (articles.size <= lastSize + 120) return;
   lastSize = articles.size;
 
   const output = Object.fromEntries(articles.entries());
-  await Deno.writeTextFile(filepath, JSON.stringify(output));
+  await Deno.writeTextFile(filepath, JSON.stringify(output, null, 2));
   console.log(articles.size, "articles checked");
+}, 120);
+
+let issues_size = issues.size;
+setInterval(async () => {
+  if (issues.size === issues_size) return;
+  issues_size = issues.size;
+
+  const output = [...issues];
+  await Deno.writeTextFile(issues_file_path, JSON.stringify(output, null, 2));
+  console.log(issues.size, "issues recorded");
 }, 1200);
